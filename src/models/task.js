68b3de8d61be0e18abcc7f7d12fe528451df4797
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-07-01 23:11:35 +0100

    Add useful properties to task objects

--- src/models/task.js
+++ src/models/task.js
@@ -1,10 +1,11 @@
 export class Task {
-  constructor({ title, description, dueDate, priority, isDone = false }) {
+  constructor({ title, description, dueDate, priority, isDone = false, dateAdded, dateCompleted }) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.isDone = isDone;
+    this.dateAdded = dateAdded;
   }
 
   toggleDone() {
