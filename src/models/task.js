de9f01e28b2cc70cc919823064781983280c6a60
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-07-07 10:13:55 +0100

    Make a change in the constructor.
    Allowing for a more flexible method of creating Task Objects.

--- src/models/task.js
+++ src/models/task.js
@@ -1,10 +1,11 @@
 export class Task {
-  constructor({ title, description, dueDate, priority, isDone = false }) {
+  constructor({ title, description, dueDate, priority, isDone = false,dateAdded }) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.isDone = isDone;
+    if (dateAdded) this.dateAdded = dateAdded;
   }
   dateAdded;
   dateCompleted;
