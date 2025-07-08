9b658a474639d009da2c8f410ad9b4c44cfc5366
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-07-08 13:07:03 +0100

    Add inbox project to local Storage.
    Thatll be where unorganized tasks will go. A default project if you will.

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,45 +1,57 @@
 import { Project } from "../models/project.js";
 import { domStuff } from "./dom-controls.js";
 
+
 export const noUserName = () => !localStorage.getItem("userName");
 domStuff.setUserName(localStorage.getItem("userName"));
 
-if (!localStorage.getItem("projects")) localStorage.setItem("projects", JSON.stringify([]));
 
 export class Storage {
-  static saveNewProject(title){
+  static saveNewProject(title) {
     const projects = JSON.parse(localStorage.getItem("projects"));
     projects.push(new Project(title));
     projects.at(-1).index = projects.length - 1;
-    localStorage.setItem("projects",JSON.stringify(projects));
+    localStorage.setItem("projects", JSON.stringify(projects));
   }
-  static getSavedProjects(){
+  static getSavedProjects() {
     return JSON.parse(localStorage.getItem("projects"));
   }
-
-  static addNewTask(projectIndex,task){
+  
+  static addNewTask(projectIndex, task) {
     const projects = JSON.parse(localStorage.getItem("projects"));
     projects[projectIndex].tasks.push(task);
-    localStorage.setItem("projects",JSON.stringify(projects));
+    localStorage.setItem("projects", JSON.stringify(projects));
   }
-  static toggleDone(projectIndex,taskIndex){
+  static toggleDone(projectIndex, taskIndex) {
     const projects = JSON.parse(localStorage.getItem("projects"));
-    projects[projectIndex].tasks[taskIndex].isDone = !projects[projectIndex].tasks[taskIndex].isDone;
+    projects[projectIndex].tasks[taskIndex].isDone =
+      !projects[projectIndex].tasks[taskIndex].isDone;
     projects[projectIndex].tasks[taskIndex].dateCompleted = new Date();
-    localStorage.setItem("projects",JSON.stringify(projects));
+    localStorage.setItem("projects", JSON.stringify(projects));
   }
-  static editTask({projectIndex,taskIndex,newTitle,newDueDate,newDescription,newPriority}){
+  static editTask({
+    projectIndex,
+    taskIndex,
+    newTitle,
+    newDueDate,
+    newDescription,
+    newPriority,
+  }) {
     const projects = JSON.parse(localStorage.getItem("projects"));
     projects[projectIndex].tasks[taskIndex].title = newTitle.trim();
     projects[projectIndex].tasks[taskIndex].dueDate = new Date(newDueDate);
     projects[projectIndex].tasks[taskIndex].description = newDescription;
     projects[projectIndex].tasks[taskIndex].priority = newPriority;
-    localStorage.setItem("projects",JSON.stringify(projects));
+    localStorage.setItem("projects", JSON.stringify(projects));
   }
-  static deleteTask(projectIndex,taskIndex){
+  static deleteTask(projectIndex, taskIndex) {
     const projects = JSON.parse(localStorage.getItem("projects"));
-    projects[projectIndex].tasks.splice(taskIndex,1);
-    localStorage.setItem("projects",JSON.stringify(projects));
+    projects[projectIndex].tasks.splice(taskIndex, 1);
+    localStorage.setItem("projects", JSON.stringify(projects));
   }
 }
-export const projects =()=> Storage.getSavedProjects();
+if (!localStorage.getItem("projects")) {
+  localStorage.setItem("projects", JSON.stringify([]));
+  Storage.saveNewProject("Inbox");
+}
+export const projects = () => Storage.getSavedProjects();
