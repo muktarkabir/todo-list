ec4645cb04a044a99d90556269ee5d73415198c4
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-07-09 16:38:20 +0100

    Display completed tasks on the page.

--- src/modules/completed-tasks-page.js
+++ src/modules/completed-tasks-page.js
@@ -1,9 +1,28 @@
+import { projects } from "./storage.js";
 import { createElement } from "./utilities.js";
+import { Project } from "../models/project.js";
+import { taskTile } from "./task-tile.js";
 
 export const completedTasksPage = () => {
   const container = createElement({ tagName: "div", className: "project-div" });
   const heading = createElement({ tagName: "h1", className: "head" });
-  heading.textContent = "Completed tasks"
+  heading.textContent = "Completed tasks";
   container.append(heading);
+
+  const renderCompletedTasks = () => {
+    projects().forEach((project, index) => {
+      const projectFromJson = Project.fromJson(project);
+      const title = createElement({ tagName: "h2" });
+      title.textContent = projectFromJson.title;
+      container.append(title);
+      projectFromJson.tasks.forEach((task) => {
+        if (task.isDone) {
+          const renderedTask = taskTile(task, index);
+          container.append(renderedTask);
+        }
+      });
+    });
+  };
+  renderCompletedTasks();
   return container;
 };
